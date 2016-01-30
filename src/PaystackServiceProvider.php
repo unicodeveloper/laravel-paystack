8116b31c8b17ae42f25155718b867a3b63952890
busayo <prosperotemuyiwa@gmail.com>
2016-01-30 22:54:23 +0100

    fix(serviceprovider): Add ability to publish config files

--- src/PaystackServiceProvider.php
+++ src/PaystackServiceProvider.php
@@ -13,8 +13,17 @@ class PaystackServiceProvider extends ServiceProvider {
    */
   protected $defer = false;
 
+  /**
+   * Publishes all the config file this package needs to function
+   * @return void
+   */
   public function boot()
   {
+        $config = realpath(__DIR__.'/../resources/config/paystack.php');
+
+        $this->publishes([
+            $config => config_path('paystack.php')
+        ]);
   }
 
   /**
