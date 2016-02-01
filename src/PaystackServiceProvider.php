9800f3783fa58fcdef91dda190f9de50b6c319cc
busayo <prosperotemuyiwa@gmail.com>
2016-02-01 08:00:09 +0100

    chore(paystack): Add attribution :credit_card:

--- src/PaystackServiceProvider.php
+++ src/PaystackServiceProvider.php
@@ -1,5 +1,15 @@
 <?php
 
+
+/*
+ * This file is part of the Laravel Paystack package.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Unicodeveloper\Paystack;
 
 use Illuminate\Support\ServiceProvider;
