472356a77d5cc28cfc366774aa23905bdf1f9586
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-18 20:53:58 +0100

    Add a new Facade Accessor

---
+++ src/Facades/Pstk.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Laravel Paystack package.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Unicodeveloper\Paystack\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Pstk extends Facade
+{
+    /**
+     * Get the registered name of the component
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'paystack';
+    }
+}
