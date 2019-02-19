a140fa367f338e75e1b0f9d48859bce9197f5e82
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-19 02:32:04 +0100

    Update Doc blocks

--- tests/PaystackFactoryTest.php
+++ tests/PaystackFactoryTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Laravel Paystack package.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Unicodeveloper\Paystack\Test;
 
 
